483d7cc1707f4ceae0087cd83227095c1672fa9b
henninggabriella <148967200+henninggabriella@users.noreply.github.com>
2024-09-26 23:46:53 -0300

    criando

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    '🛹',
+    'Quem é Bob Burnquist?',
+    'Bob Burnquist é um profissional do skate que está entre os melhores do mundo.'
+);
+
+criaCartao(
+    '🛹',
+    'Quando e onde Bob Burnquist nasceu?',
+    'Bob nasceu no Rio de Janeiro, morou até a adolescência em São Paulo e partiu para os Estados Unidos ainda jovem.'
+);
+
+criaCartao(
+    '🛹',
+    'Qual é a especialidade de Bob Burnquist no skate?',
+    'Bob é conhecido por suas manobras no vert e por ser um pioneiro em manobras de "skate na mega-rampa".'
+);
+
+criaCartao(
+    '🛹',
+    'Qual foi um dos primeiros truques que Bob Burnquist popularizou?',
+    'Ele é famoso por popularizar o "Ollie" em rampas verticais.'
+);
+
+criaCartao(
+    '🛹',
+    'Quais prêmios Bob Burnquist já ganhou?',
+    'Bob ganhou múltiplas medalhas de ouro nos X Games e é reconhecido globalmente.'
+);
+
+criaCartao(
+    '🛹',
+    'Qual é a contribuição de Bob Burnquist para o skate?',
+    'Bob é conhecido por sua criatividade e inovação nas manobras, influenciando gerações de skatistas.'
+);
